495ce0c6f793cea08844f3f3ace357d44ffc32ce
David MT Wong <mingtak.wong1986@gmail.com>
2017-03-27 02:44:44 +0800

    Add comments for functions to describe behaviors

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,11 @@
 ## functions do
 
 ## Write a short comment describing this function
+# makeCacheMatrix expect a square matrix as input; if nothing is provided, an 
+# empty matrix is created; there are four functions belong to this function 
+# which are meant to manipulate the matrix and give inverse
+# $set and $ get for set and return the matrix, 
+# $setInv and $getInv for set and return the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -19,6 +24,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+# cacheSolve expect resulting object of makeCacheMatrix as input; 
+# should inverse of the matrix (m) are not null, cacheSolve will simply 
+# return msg 'getting cached data' and then the existing inverse matrix without
+# fruther computed again. If not use $setInv which is an element of makeCacheMatrix
+# to compute and return inverse of matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -33,3 +43,8 @@ cacheSolve <- function(x, ...) {
   m
 }
 
+#tmp$get()
+#tmp$set(diag(3))
+#tmp$getInv()
+#cacheSolve(tmp)
+#tmp$setInv(diag(4))
